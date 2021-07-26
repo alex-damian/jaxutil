e867f0371a359e695d5b2da7e47eabced8a0a43f
Alex Damian <9121265+adamian98@users.noreply.github.com>
2021-07-26 14:03:05 -0400

    fixed whitespace

--- jaxbase/data/torch/cifar10.py
+++ jaxbase/data/torch/cifar10.py
@@ -9,35 +9,34 @@ def OneHot(x):
 	return np.eye(10)[x]
 	
 def CIFAR10_Normalize(x):
-    x = np.asarray(x, dtype=np.float32)
-    x = (x - CIFAR10_MEAN) / CIFAR10_STD
-    return x
+	x = np.asarray(x, dtype=np.float32)
+	x = (x - CIFAR10_MEAN) / CIFAR10_STD
+	return x
 
 def numpy(data_dir, download=False, normalize=True, one_hot=True):
-    traindata = torchvision.datasets.CIFAR10(data_dir, train=True, download=download)
-    testdata = torchvision.datasets.CIFAR10(data_dir, train=False, download=download)
-    train_x, train_y, test_x, test_y = traindata.data, traindata.targets, testdata.data, testdata.targets
-    cifar10_mean, cifar10_std = train_x.mean(), train_x.std()
-    if normalize:
-        train_x, test_x = CIFAR10_Normalize(train_x), CIFAR10_Normalize(test_x)
-    if one_hot:
-        train_y, test_y = OneHot(train_y), OneHot(test_y)
-    return train_x,train_y,test_x,test_y
+	traindata = torchvision.datasets.CIFAR10(data_dir, train=True, download=download)
+	testdata = torchvision.datasets.CIFAR10(data_dir, train=False, download=download)
+	train_x, train_y, test_x, test_y = traindata.data, traindata.targets, testdata.data, testdata.targets
+	if normalize:
+		train_x, test_x = CIFAR10_Normalize(train_x), CIFAR10_Normalize(test_x)
+	if one_hot:
+		train_y, test_y = OneHot(train_y), OneHot(test_y)
+		return train_x,train_y,test_x,test_y
 
 def torch(data_dir, download=False, normalize=True, one_hot=True, data_aug=False):
 	normalizations = [CIFAR10_Normalize] if normalize else []
-    augmentations = [T.RandomCrop(32, padding=4),T.RandomHorizontalFlip()] if data_aug else []
-    transform_train = T.Compose(augmentations + normalizations)
-    transform_test = T.Compose(normalizations)
+	augmentations = [T.RandomCrop(32, padding=4),T.RandomHorizontalFlip()] if data_aug else []
+	transform_train = T.Compose(augmentations + normalizations)
+	transform_test = T.Compose(normalizations)
 	target_transform = OneHot if one_hot else None
-    traindata = torchvision.datasets.CIFAR10(data_dir,
-											transform=transform_train,
-											target_transform=target_transform,
-											train=True, 
-											download=download)
-    testdata = torchvision.datasets.CIFAR10(data_dir,
-											transform=transform_test,
-											target_transform=target_transform,
-											train=False,
-											download=download)
-    return traindata, testdata
+	traindata = torchvision.datasets.CIFAR10(data_dir,
+						 transform=transform_train,
+						 target_transform=target_transform,
+						 train=True, 
+						 download=download)
+	testdata = torchvision.datasets.CIFAR10(data_dir,
+						transform=transform_test,
+						target_transform=target_transform,
+						train=False,
+						download=download)
+	return traindata, testdata
