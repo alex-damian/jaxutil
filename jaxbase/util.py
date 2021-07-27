c0cfbe33c17081fe3edde12d87eaa6b9a6a2a6d6
Alex Damian <9121265+adamian98@users.noreply.github.com>
2021-07-27 11:37:13 -0400

    added batch_split function

--- jaxbase/util.py
+++ jaxbase/util.py
@@ -1,5 +1,15 @@
+import jax
+
 def clean_dict(d):
     return {key:val.item() for key,val in d.items()}
 
 def Identity(*args,**kwargs):
     return lambda x: x
+
+def batch_split(x,devices):
+    if isinstance(devices,list):
+        n_devices = len(devices)
+    elif isinstance(devices,int):
+        n_devices = devices
+    else: raise Exception("Unknown device list")
+    return jax.tree_map(lambda x: x.reshape((n_devices,-1,*x.shape[1:])), x)
