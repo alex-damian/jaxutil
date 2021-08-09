c89aeaa80e0af730f0ba7bb12b253a924de1441b
adamian98 <ad27@princeton.edu>
2021-08-09 14:13:15 +0000

    updated version number

--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "jaxbase",
+    name="jaxbase",
     packages=find_packages(),
-    version = "0.1.2",
-    author = "Alex Damian",
-    author_email = "ad27@princeton.edu",
-    description = "Jax utility package for common models and datasets",
-    license = "MIT",
-    keywords = "jax",
+    version="0.2.0",
+    author="Alex Damian",
+    author_email="ad27@princeton.edu",
+    description="Jax utility package for common models and datasets",
+    license="MIT",
+    keywords="jax",
     # url = "http://packages.python.org/an_example_pypi_project",
-    install_requires=[
-        'jax',
-        'flax',
-        'tensorflow-datasets',
-        'jax-resnet'
-    ],
+    install_requires=["jax", "flax", "tensorflow-datasets", "jax-resnet"],
 )
