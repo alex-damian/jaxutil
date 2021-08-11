2e4d2215ac2097f18e1cc72420488be03123cec0
adamian98 <ad27@princeton.edu>
2021-08-11 16:49:15 +0000

    added laxmean to util.py

--- jaxbase/util.py
+++ jaxbase/util.py
@@ -1,15 +1,29 @@
 import jax
+from jax import numpy as jnp
+from jax import lax
+
 
 def clean_dict(d):
-    return {key:val.item() for key,val in d.items()}
+    return {key: val.item() for key, val in d.items()}
 
-def Identity(*args,**kwargs):
+
+def Identity(*args, **kwargs):
     return lambda x: x
 
-def batch_split(x,devices):
-    if isinstance(devices,list):
+
+def batch_split(x, devices):
+    if isinstance(devices, list):
         n_devices = len(devices)
-    elif isinstance(devices,int):
+    elif isinstance(devices, int):
         n_devices = devices
-    else: raise Exception("Unknown device list")
-    return jax.tree_map(lambda x: x.reshape((n_devices,-1,*x.shape[1:])), x)
+    else:
+        raise Exception("Unknown device list")
+    return jax.tree_map(lambda x: x.reshape((n_devices, -1, *x.shape[1:])), x)
+
+
+def laxmean(f, x, out_shape):
+    def _step(s, xi):
+        s += f(xi) / len(x)
+        return s, None
+
+    return lax.scan(_step, jnp.zeros(out_shape), x)[0]
