dcc16d976d98f8b517b9d036898cbe8c839d1ae6
Alex Damian <9121265+adamian98@users.noreply.github.com>
2021-08-14 19:54:41 -0400

    Update mnist.py

--- jaxbase/data/torch/mnist.py
+++ jaxbase/data/torch/mnist.py
@@ -11,10 +11,10 @@ def numpy(data_dir, download=False, flatten=True, one_hot=True, normalize=True):
 	traindata = torchvision.datasets.MNIST(data_dir, train=True, download=download)
 	testdata = torchvision.datasets.MNIST(data_dir, train=False, download=download)
 	train_x, train_y, test_x, test_y = traindata.data, traindata.targets, testdata.data, testdata.targets
+	if normalize:
+		train_x, test_x = np.array(train_x,dtype=np.float32)/255, np.array(test_x,dtype=np.float32)/255
 	if flatten:
 		train_x, test_x = Flatten(train_x), Flatten(test_x)
 	if one_hot:
 		train_y, test_y = OneHot(train_y), OneHot(test_y)
-	if normalize:
-		train_x, test_x = np.array(train_x,dtype=np.float32)/255, np.array(test_x,dtype=np.float32)/255
 	return train_x,train_y,test_x,test_y
