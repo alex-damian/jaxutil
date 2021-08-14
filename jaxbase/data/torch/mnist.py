d936809ffc8d0e570a31990f90345d71bbf3b398
Alex Damian <9121265+adamian98@users.noreply.github.com>
2021-08-14 13:47:03 -0400

    Update mnist.py

--- jaxbase/data/torch/mnist.py
+++ jaxbase/data/torch/mnist.py
@@ -16,5 +16,5 @@ def numpy(data_dir, download=False, flatten=True, one_hot=True, normalize=True):
 	if one_hot:
 		train_y, test_y = OneHot(train_y), OneHot(test_y)
 	if normalize:
-		train_x, test_x = train_x/np.float32(255), test_x/np.float32(255)
+		train_x, test_x = np.array(train_x,dtype=np.float32)/255, np.array(test_x,dtype=np.float32)/255
 	return train_x,train_y,test_x,test_y
