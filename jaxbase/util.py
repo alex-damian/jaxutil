3f3840d45ffe0a8b163282c83d73777935f1fca8
adamian98 <ad27@princeton.edu>
2021-08-19 16:51:59 +0000

    fixed laxmean to work with pytrees

--- jaxbase/util.py
+++ jaxbase/util.py
@@ -23,8 +23,9 @@ def batch_split(x, devices):
 
 def laxmean(f, x, out_shape):
     n = len(jax.tree_leaves(x)[0])
+
     def _step(s, xi):
-        s += f(xi)/n
+        s = jax.tree_multimap(lambda si, fi: si + fi / n, s, f(xi))
         return s, None
 
     return lax.scan(_step, jnp.zeros(out_shape), x)[0]
