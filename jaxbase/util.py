ae6cdb75d1f09021937bee9a30db0e0a6dbd3fad
Alex Damian <adamian98@users.noreply.github.com>
2021-09-18 21:39:36 -0400

    fixed bug

--- jaxbase/util.py
+++ jaxbase/util.py
@@ -25,10 +25,10 @@ def batch_split(batch, n_batch=None, batch_size=None, strict=True):
         raise Exception("Need to specify either n_batch or batch_size")
 
     if strict:
-        assert n_devices*batch_size == n
+        assert n_batch*batch_size == n
     else:
         batch = jax.tree_map(lambda x: x[:n_devices*batch_size], batch)
-    batches = jax.tree_map(lambda x: x.reshape((n_devices, batch_size, *x.shape[1:])), batch)
+    batches = jax.tree_map(lambda x: x.reshape((n_batch, batch_size, *x.shape[1:])), batch)
     return batches
 
 
