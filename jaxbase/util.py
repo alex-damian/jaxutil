fb2497c568a5351bb39bb066cb7c238cd4e91587
Alex Damian <adamian98@users.noreply.github.com>
2021-09-18 21:14:26 -0400

    updated batch_split

--- jaxbase/util.py
+++ jaxbase/util.py
@@ -12,14 +12,24 @@ def Identity(*args, **kwargs):
     return lambda x: x
 
 
-def batch_split(x, devices):
-    if isinstance(devices, list):
-        n_devices = len(devices)
-    elif isinstance(devices, int):
-        n_devices = devices
+def batch_split(batch, n_batch=None, batch_size=None, strict=True):
+    n = len(jax.tree_leaves(batch)[0])
+    if isinstance(n_batch, list):
+        n_batch = len(devices)
+        batch_size = n//n_batch
+    elif isinstance(n_batch, int):
+        batch_size = n//n_batch
+    elif: isinstance(batch_size, int):
+        n_batch = n//batch_size
     else:
-        raise Exception("Unknown device list")
-    return jax.tree_map(lambda x: x.reshape((n_devices, -1, *x.shape[1:])), x)
+        raise Exception("Need to specify either n_batch or batch_size")
+
+    if strict:
+        assert n_devices*batch_size = n
+    else:
+        batch = jax.tree_map(lambda x: x[:n_devices*batch_size], batch)
+    batches = jax.tree_map(lambda x: x.reshape((n_devices, batch_size, *x.shape[1:])), batch)
+    return batches
 
 
 def laxmean(f, x, init):
