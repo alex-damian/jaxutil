44ad0dc79a333f526ea26baa4700cc3a3309effa
Alex Damian <ad27@princeton.edu>
2022-02-16 21:00:38 -0500

    added learning rate

--- jaxbase/train.py
+++ jaxbase/train.py
@@ -3,13 +3,14 @@ from jax import numpy as jnp
 from jaxbase.util import fold
 
 
-def GD(loss, p0, steps, return_loss=True, **kwargs):
+def GD(loss, p0, lr, steps, return_loss=True, **kwargs):
     def step(p, _):
         if return_loss:
             step_loss, grads = jax.value_and_grad(loss)(p)
         else:
             step_loss = None
             grads = jax.grad(loss)(p)
+        p = jax.tree_multimap(lambda pi, gi: pi - lr * gi, p, grads)
         return p, step_loss, None
 
     fold_output = fold(step, p0, jnp.arange(steps), **kwargs)
