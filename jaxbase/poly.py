944c257ee91f4a2ed129aeea02421840f0a4547b
Alex Damian <ad27@princeton.edu>
2022-02-16 19:36:13 -0500

    added factorial function

--- jaxbase/poly.py
+++ jaxbase/poly.py
@@ -3,4 +3,6 @@ import jax.numpy as jnp
 import numpy as np
 from numpy.polynomial.hermite_e import herme2poly
 
+factorial = lambda k: jnp.prod(jnp.arange(k, 0, -1))
+factorial2 = lambda k: jnp.prod(jnp.arange(k, 0, -2))
 He = lambda k, x: jnp.polyval(herme2poly([0] * k + [1])[::-1], x)
