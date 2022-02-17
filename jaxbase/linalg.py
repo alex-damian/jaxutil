7a01c7a438e3d03be2013d9bbdd3714e5a364d5d
Alex Damian <ad27@princeton.edu>
2022-02-16 20:01:58 -0500

    updated precision

--- jaxbase/linalg.py
+++ jaxbase/linalg.py
@@ -3,10 +3,18 @@ from jax.numpy import linalg as jla
 
 
 def ridge(x, y, reg):
-    U, S, V = jla.svd(x, full_matrices=False)
-    UTy = U.T @ y
+    dtype = x.dtype
+    m, n = x.shape
+    rcond = jnp.finfo(dtype).eps * max(n, m)
+    u, s, vt = jla.svd(x, full_matrices=False)
+    uTy = jnp.matmul(u.conj().T, y, precision=lax.Precision.HIGHEST)
 
-    ridge_problem = lambda reg: V.T @ ((S / (S * S + reg)) * UTy)
+    def ridge_problem(reg):
+        mask = s * s + reg >= (rcond * s[0]) ** 2
+        safe_s = jnp.where(mask, s, 1)
+        s_ridge = jnp.where(mask, safe_s / (safe_s**2 + reg), 0)[:, jnp.newaxis]
+        return jnp.matmul(vt.conj().T, s_ridge * uTy, precision=lax.Precision.HIGHEST)
+
     if jnp.ndim(reg) == 0:
         return ridge_problem(reg)
     else:
