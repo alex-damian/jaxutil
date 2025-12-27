0712f2dd7d2cda1b59a6b2220361200029e93542
Alex Damian <alexemonad@gmail.com>
2025-12-27 00:01:04 -0500

    removed types

--- jaxutil/__init__.py
+++ jaxutil/__init__.py
@@ -2,20 +2,4 @@ from .derivatives import diff, smart_jacobian
 from .lax_util import batch_split, fold, laxmap, laxmean, laxsum, tree_stack
 from .linalg import cos_dist, get_ridge_fn, orthogonalize
 from .poly import He, factorial, factorial2
-from .types import (
-    bf16,
-    c64,
-    c128,
-    f16,
-    f32,
-    f64,
-    i8,
-    i16,
-    i32,
-    i64,
-    ui8,
-    ui16,
-    ui32,
-    ui64,
-)
 from .util import RNG, ddict, tree_to_dict
