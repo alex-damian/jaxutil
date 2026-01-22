f93e0d9b59624f6a13c57015e630ab795fef6530
Alex Damian <alexemonad@gmail.com>
2026-01-22 17:34:07 -0500

    added some imports

--- jaxutil/auto.py
+++ jaxutil/auto.py
@@ -1,19 +1,23 @@
+import os
+import sys
 from collections import namedtuple
 from functools import partial
+from pathlib import Path
+from time import perf_counter
 from typing import Any, Callable, List, Optional, Union
 
 import equinox as eqx
-from jaxtyping import Array, Float, Int, PRNGKeyArray, PyTree
 import jax
 import jax.numpy.linalg as jla
+import numpy as np
 import optax
-from jax import jit, lax, vmap, nn
+from jax import jit, lax, nn, vmap
 from jax import numpy as jnp
 from jax import random as jr
 from jax import tree_util as jtu
 from jax.flatten_util import ravel_pytree
+from jaxtyping import Array, Float, Int, PRNGKeyArray, PyTree
 from matplotlib import pyplot as plt
 from numpy import linalg as la
-import numpy as np
 
 from jaxutil import *
