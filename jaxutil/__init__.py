4de95bf085978296557876028de7e8d11a5c30ae
Alex Damian <alexemonad@gmail.com>
2026-01-26 10:46:14 -0500

    added GPU flags

--- jaxutil/__init__.py
+++ jaxutil/__init__.py
@@ -2,4 +2,4 @@ from .derivatives import diff, smart_jacobian
 from .lax_util import batch_split, fold, laxmap, laxmean, laxsum, tree_stack
 from .linalg import cos_dist, get_ridge_fn, orthogonalize
 from .poly import He, factorial, factorial2
-from .util import RNG, ddict, tree_to_dict
+from .util import RNG, ddict, enable_maxtext_gpu_flags, tree_to_dict
--- jaxutil/util.py
+++ jaxutil/util.py
@@ -52,3 +52,30 @@ def tree_to_dict(pytree):
         jax.tree_util.keystr(k, simple=True, separator="."): v
         for k, v in jax.tree.leaves_with_path(pytree)
     }
+
+
+def enable_maxtext_gpu_flags():
+    import os
+    import sys
+
+    if "jax" in sys.modules:
+        raise RuntimeError(
+            "enable_maxtext_gpu_flags() must be called before importing JAX."
+        )
+
+    os.environ["XLA_FLAGS"] = (
+        "--xla_gpu_enable_latency_hiding_scheduler=true "
+        "--xla_gpu_enable_triton_gemm=false "
+        "--xla_gpu_enable_command_buffer='' "
+        "--xla_gpu_enable_highest_priority_async_stream=true "
+        "--xla_gpu_all_reduce_combine_threshold_bytes=134217728 "
+        "--xla_gpu_all_gather_combine_threshold_bytes=134217728 "
+        "--xla_gpu_reduce_scatter_combine_threshold_bytes=67108864 "
+        "--xla_gpu_enable_pipelined_all_gather=true "
+        "--xla_gpu_enable_pipelined_reduce_scatter=true "
+        "--xla_gpu_enable_pipelined_all_reduce=true "
+        "--xla_gpu_enable_while_loop_double_buffering=true "
+        "--xla_gpu_enable_all_gather_combine_by_dim=false "
+        "--xla_gpu_enable_reduce_scatter_combine_by_dim=false "
+        "--xla_disable_hlo_passes=rematerialization"
+    )
