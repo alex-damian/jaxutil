3523be502ba75a9858c7b887a0a24e2f0e27a3d9
Alex Damian <alexemonad@gmail.com>
2026-03-05 13:36:47 -0500

    exported xmap

--- src/jaxutil/__init__.py
+++ src/jaxutil/__init__.py
@@ -1,5 +1,5 @@
 from .derivatives import diff, smart_jacobian
-from .lax_util import batch_split, fold, laxmap, laxmean, laxsum, tree_stack
+from .lax_util import batch_split, fold, laxmap, laxmean, laxsum, tree_stack, xmap
 from .linalg import cos_dist, get_ridge_fn, orthogonalize
 from .poly import He, factorial, factorial2
 from .util import RNG, ddict, tree_to_dict
